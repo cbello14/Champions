b53895bf9f6028290edf0e97c311af02aee696f6
DangerousDB <danielblumkine@gmail.com>
2026-02-13 14:00:37 -0500

    Added header to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import { Outlet } from "react-router";
+import { Outlet, NavLink } from "react-router";
 import './index.css'
 function App() {
 
 	return (
 		<>
 			<div className="h-screen w-full bg-background text-foreground">
+				<header className="flex justify-between gap-4 p-4">
+					<p>Champions</p>
+					<NavLink to="/" end>
+						Home
+					</NavLink>
+				</header>
 				<Outlet />
 			</div>
 		</>
--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -3,10 +3,15 @@ import { useNavigate } from "react-router";
 const HomePage = () => {
 	const navigate = useNavigate();
 	return <>
-		<Button onClick={() => { navigate("games"); }}> Create Custom Game</Button>
-		<Button onClick={() => { navigate("pieces"); }}> Create Custom Piece</Button>
-		<Button onClick={() => { navigate("boards"); }}> Create Custom Board</Button>
-		<Button onClick={() => { navigate("start"); }}> Start A Game </Button>
+		<div className="flex-row justify-between">
+			<div className="flex-col">
+				<Button onClick={() => { navigate("games"); }}> Create Custom Game</Button>
+				<Button onClick={() => { navigate("pieces"); }}> Create Custom Piece</Button>
+				<Button onClick={() => { navigate("boards"); }}> Create Custom Board</Button>
+				<Button onClick={() => { navigate("start"); }}> Start A Game </Button>
+			</div>
+			<img src="https://www.regencychess.co.uk/images/how-to-set-up-a-chessboard/how-to-set-up-a-chessboard-7.jpg" />
+		</div>
 	</>
 
 
