d1f6ec0e1e2d1a3848773426a3f8e9c4e1237c89
DangerousDB <danielblumkine@gmail.com>
2026-02-18 21:23:52 -0500

    Custom Game page

--- src/components/GamePage.tsx
+++ src/components/GamePage.tsx
@@ -1,20 +1,26 @@
 import { Popover, PopoverTrigger, PopoverContent } from "@/components/ui/popover"
+import { useState } from "react"
 import { Button } from "@/components/ui/button"
 const GamePage = () => {
+
+	const [piecesOpen, setPiecesOpen] = useState<boolean>(true);
+	const [boardsOpen, setBoardsOpen] = useState<boolean>(true);
+
 	return <>
-		<div className="flex justify-between h-full gap-4 p-4">
-			<Popover open={true}>
+		<div className="flex flex-row justify-between h-full gap-4 p-4">
+			<Popover open={boardsOpen}>
 				<PopoverTrigger asChild>
 					<Button
+						onClick={() => { setBoardsOpen(!boardsOpen) }}
 						variant="ghost"
 						size="icon"
-						className="data-[state=open]:bg-accent h-7 w-7"
+						className="data-[state=open]:bg-accent h-7 w-7 grow-1"
 					>
 						Boards
 					</Button>
 				</PopoverTrigger>
 				<PopoverContent
-					className="w-56 overflow-hidden rounded-lg p-0"
+					className="overflow-hidden rounded-lg p-5 grow-1"
 					align="start"
 				>
 					<div >
@@ -23,24 +29,25 @@ const GamePage = () => {
 				</PopoverContent>
 			</Popover>
 
-			<main className="flex-1 flex items-center justify-center">
-				<div className="text-center">
-					<h3>The Game</h3>
+			<main className="flex grow-5 items-center justify-center">
+				<div className="center">
+					<img src="https://www.regencychess.co.uk/images/how-to-set-up-a-chessboard/how-to-set-up-a-chessboard-7.jpg" />
 				</div>
 			</main>
 
-			<Popover open={true}>
+			<Popover open={piecesOpen}>
 				<PopoverTrigger asChild>
 					<Button
+						onClick={() => { setPiecesOpen(!piecesOpen) }}
 						variant="ghost"
 						size="icon"
-						className="data-[state=open]:bg-accent h-7 w-7"
+						className="data-[state=open]:bg-accent h-7 w-7 grow-1"
 					>
 						Pieces
 					</Button>
 				</PopoverTrigger>
 				<PopoverContent
-					className="w-56 overflow-hidden rounded-lg p-0"
+					className="overflow-hidden rounded-lg p-5 grow-1"
 					align="end"
 				>
 					<div >
