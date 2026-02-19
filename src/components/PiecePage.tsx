1971311efb69b7b6bcbc07703ede849fbf73b052
DangerousDB <danielblumkine@gmail.com>
2026-02-19 15:41:28 -0500

    Moved sidebars into a seperate component

--- src/components/PiecePage.tsx
+++ src/components/PiecePage.tsx
@@ -1,32 +1,13 @@
 import { useState } from "react"
-import { Popover, PopoverTrigger, PopoverContent } from "@/components/ui/popover"
 import { Button } from "@/components/ui/button";
+import SideBar from "@/components/SideBar.tsx";
 const PiecePage = () => {
 
 	const [piecesOpen, setPiecesOpen] = useState<boolean>(true);
 
 	return <>
 		<div className="flex flex-row justify-between gap-4 p-4 items-start">
-			<Popover open={piecesOpen}>
-				<PopoverTrigger asChild>
-					<Button
-						onClick={() => { setPiecesOpen(!piecesOpen) }}
-						variant="ghost"
-						size="icon"
-						className="data-[state=open]:bg-accent h-7 w-7 grow-1"
-					>
-						Pieces
-					</Button>
-				</PopoverTrigger>
-				<PopoverContent
-					className="overflow-hidden rounded-lg p-5 grow-1"
-					align="start"
-				>
-					<div >
-						<h2 >Pieces would go here</h2>
-					</div>
-				</PopoverContent>
-			</Popover>
+			<SideBar isOpen={piecesOpen} setIsOpen={(state: boolean) => { setPiecesOpen(state) }} name={"Pieces"} content={<h2> Pieces would go here </h2>} align={"left"} />
 			<main className="flex grow-5 items-center justify-center">
 				<div className="flex flex-col center">
 					<img src="https://www.regencychess.co.uk/images/how-to-set-up-a-chessboard/how-to-set-up-a-chessboard-7.jpg" />
