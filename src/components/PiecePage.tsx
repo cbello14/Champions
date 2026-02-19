d7f0e0bcf4db947557a10cae9e1f3eb87bbb2ee8
DangerousDB <danielblumkine@gmail.com>
2026-02-18 21:38:55 -0500

    Pieces Page

--- src/components/PiecePage.tsx
+++ src/components/PiecePage.tsx
@@ -1,7 +1,42 @@
+import { useState } from "react"
+import { Popover, PopoverTrigger, PopoverContent } from "@/components/ui/popover"
+import { Button } from "@/components/ui/button";
 const PiecePage = () => {
 
+	const [piecesOpen, setPiecesOpen] = useState<boolean>(true);
 
-return <> This is the pieces tab </>
+	return <>
+		<div className="flex flex-row justify-between gap-4 p-4 items-start">
+			<Popover open={piecesOpen}>
+				<PopoverTrigger asChild>
+					<Button
+						onClick={() => { setPiecesOpen(!piecesOpen) }}
+						variant="ghost"
+						size="icon"
+						className="data-[state=open]:bg-accent h-7 w-7 grow-1"
+					>
+						Pieces
+					</Button>
+				</PopoverTrigger>
+				<PopoverContent
+					className="overflow-hidden rounded-lg p-5 grow-1"
+					align="start"
+				>
+					<div >
+						<h2 >Pieces would go here</h2>
+					</div>
+				</PopoverContent>
+			</Popover>
+			<main className="flex grow-5 items-center justify-center">
+				<div className="center">
+					<img src="https://www.regencychess.co.uk/images/how-to-set-up-a-chessboard/how-to-set-up-a-chessboard-7.jpg" />
+				</div>
+			</main>
+			<div className="flex grow-1 flex-none w-48">
+				<img className="flex grow-1 object-scale-down" src="https://img.freepik.com/free-vector/chess_53876-25642.jpg?semt=ais_hybrid&w=740&q=80" />
+			</div>
+		</div >
+	</>
 }
 
 export default PiecePage
