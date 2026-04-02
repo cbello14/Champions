f76819a444fba8f65349f0429dcaa05cfb858bbf
SebastienB <brandjsebastien@gmail.com>
2026-04-02 15:55:59 -0400

    directional pawns!
    pawns for team 3 and 4 are now distinct from black pawns.
    also got rid of some old comments

--- src/types/capture.ts
+++ src/types/capture.ts
@@ -4,20 +4,3 @@ type capture = 'direct' | 'no-capture' | movement[]
 
 export type { capture }
 
-
-/*
-Possible class version
-
-export class Capture {
-	direction: string;
-	condition: boolean;
-	static genericConstruct(){
-		return new Move("$o|1^",true);
-	}
-	constructor(directions: string,conditions: boolean){
-		this.direction=directions
-		this.condition=conditions
-	}
-};
-
-*/
