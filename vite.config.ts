96dfcee0d7c0389af71975ddd3b09559d878e465
DangerousDB <danielblumkine@gmail.com>
2026-04-16 20:12:18 -0400

    configured so that ngrok can host the site

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+	plugins: [react(), tailwindcss()],
+	server: {
+		allowedHosts: ['.ngrok-free.app'],
+	},
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
 })
